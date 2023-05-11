547034aef3c26c31777eb466221976f8021e7cc5
Gustav68 <zubakv@email.cz>
2023-05-11 17:41:53 +0200

    update

--- BackgroundColor.py
+++ BackgroundColor.py
@@ -1,30 +1,18 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
+def change_background_color(source, background_color, output):
+    
+    picture = Image.open(source).convert("RGBA")
+    picture_update = Image.new("RGBA", picture.size, background_color)
+    picture_update.paste(picture, (0, 0), picture)
+    picture_update.save(output)  # Save the modified image to a file
+    picture_update.show()  # Display the modified image
+    
 def main():
-    path = "C:\\Users\\vlada\\Documents\\Python\\BackgroundColor\\ATD401.jpg"
-    picture = Image.open(path)
-    
-    width, height = picture.size
-    x = 0
-    while x < width:
-        y = 0
-        while y < height:
-        #max r,g,b = 255, min = 0
-            r, g, b = picture.getpixel((x,y))
-            average = int((r+g+b)/3)
-        #obrazek.putpixel((x,y), (g, b, r))
-        #obrazek.putpixel((x,y), (prumer, prumer, prumer))
-        #obrazek.putpixel((x,y), (255-prumer, 255-prumer, 255-prumer))
-            if average > 127:
-                picture.putpixel((x,y), (r+10, g+10, b+10))
-            #picture.putpixel((x,y), (255, 255, 255))
-            else:
-                picture.putpixel((x,y), (r-10, g-10, b-10))
-            #picture.putpixel((x,y), (0, 0, 0))
-                y += 1
-        x += 1
-    
-    picture.show() #obrazek.show()
+    path_source = "C:\\Users\\vlada\\Documents\\Python\\BackgroundColor\\ATD401.png"
+    path_target = "C:\\Users\\vlada\\Documents\\Python\\BackgroundColor\\ATD401_update.png"
+    background = (10, 30, 50)
+    change_background_color(path_source, background, path_target)
 
 if __name__ == "__main__":
     main()
